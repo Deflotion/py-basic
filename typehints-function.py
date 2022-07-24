9af6e3bdbb95da7e51347bcdea0637918594b935
Deflotion <danny27revaldo@gmail.com>
2022-07-24 21:31:30 +0700

    typehints-py
    berfungsi sebagai indentifikasi dalam suatu function sperti, function ini hanya bisa diisi oleh int,string dkk

--- typehints-function.py
+++ typehints-function.py
@@ -1,2 +1,26 @@
 '''Type Hints pada function'''
+import string
+
+#! bentuk standar function
+#* studi kasus
+# def fungsi(parameter):
+#     hasil = parameter**2
+#     return hasil
+
+# fungsi(1)
+# fungsi("Ucup")
+# fungsi(True)
+
+#$ penggunaan type hints
+def sepuluh_pangkat(argument:int) -> int:
+    pangkat = argument**2
+    return pangkat
+
+total = sepuluh_pangkat(5)
+print(total)
+
+def display(argument:string):
+    print(argument)
+
+display("ucok")
 
